e647025ca8ff36bafc0e29695383dddd5d2cfda7
Aaron de Miranda Colaco <aaroncolaco.work@gmail.com>
2020-03-25 16:33:11 +0530

    Comment test code

--- src/users/service.ts
+++ src/users/service.ts
@@ -16,9 +16,9 @@ async function getUser(where) {
   return User.findOne(where)
 }
 
-addUserSubscription({name: 'Jane Doe', email: 'asfsdfsd', phone:'1234567891', states: ['GA'], country: 'IN'}).catch(console.error)
-changeSubscription('5e7a62477aeda9be594f12b6', true, false).then(() =>
-  getUser({ _id: '5e7a62477aeda9be594f12b6' }).then(console.log))
+// addUserSubscription({name: 'Jane Doe', email: 'asfsdfsd', phone:'1234567891', states: ['GA'], country: 'IN'}).catch(console.error)
+// changeSubscription('5e7a62477aeda9be594f12b6', true, false).then(() =>
+//   getUser({ _id: '5e7a62477aeda9be594f12b6' }).then(console.log))
 
 
 async function changeSubscription(userID: string, emailSubscribed: boolean, phoneSubscribed: boolean) {
