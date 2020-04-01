4154d559905d27e044c6b7cb0a1161b5ad8729be
Aaron de Miranda Colaco <aaroncolaco.work@gmail.com>
2020-04-02 01:55:40 +0530

    Remove comments

--- notifier/lib/notifier/application.ex
+++ notifier/lib/notifier/application.ex
@@ -1,20 +1,15 @@
 defmodule Notifier.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Notifier.Worker.start_link(arg)
       {Notifier.StatsServer, []},
       {DynamicSupervisor, strategy: :one_for_one, name: Notifier.DynamicSupervisor},
       {Notifier.DB, []}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Notifier.Supervisor]
     Supervisor.start_link(children, opts)
   end
