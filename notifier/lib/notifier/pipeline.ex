ee9ae66727745dc0d1b6742f374d064c7ad0995c
Aaron de Miranda Colaco <aaroncolaco.work@gmail.com>
2020-04-06 21:02:24 +0530

    Increase process timeout to 3 secs

--- notifier/lib/notifier/pipeline.ex
+++ notifier/lib/notifier/pipeline.ex
@@ -19,7 +19,7 @@ defmodule Notifier.Pipeline do
     :ok =
       Notifier.SMS.send_stat_sms(user["phone"], user["pincode"], user["state"], user["country"])
 
-    Process.sleep(1000)
+    Process.sleep(3000)
 
     message
   end
